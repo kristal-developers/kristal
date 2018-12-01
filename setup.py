b86c2c3d22941d84098c8d9d6b7a12cf7c20dea0
Dexter <dexter2206@gmail.com>
2018-12-01 19:29:22 +0100

    Dev/initial project structure (#1)
    * Add README file
    
    * Add stub for main package
    
    * Add basic setup file
    
    * Add installation instructions
    
    * Fix markup in README.rst

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'Programming Language :: Python',
+    'Intended Audience :: Science/Research'
+]
+
+with open('README.rst') as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='kristal',
+    license='MIT',
+    description=__doc__,
+    long_description=LONG_DESCRIPTION,
+    install_requires=['numpy'],
+    author='Magdalena Krzuś, Konrad Jałowiecki',
+    author_email='magdalena.krzus@gmail.com',
+    packages=find_packages(exclude=['tests']))
